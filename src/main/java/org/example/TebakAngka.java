b56aa0bc36b49b7ecc15df5e03409ffe990a430f
yahya wijaya <yahyawijaya082@gmail.com>
2023-05-27 20:13:15 +0700

    menambahkan tebak angka

---
+++ src/main/java/org/example/TebakAngka.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TebakAngka {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        boolean kalah = true;
+        int angkaRandom = random.nextInt(10);
+        System.out.println("masukan angka");
+
+        for (int i =1; i<=3;i++){
+            int angka = scan.nextInt();
+            if (angka==angkaRandom){
+                System.out.println("RIGHT");
+                System.out.println("kamu memenangkan game ini");
+                kalah = false;
+                break;
+            }else if (Math.abs(angka-angkaRandom) == 1){
+                System.out.println("hot");
+            }else if (Math.abs(angka-angkaRandom) == 2){
+                System.out.println("warm");
+            }else {
+                System.out.println("cold");
+            }
+        }
+        if (kalah==true) {
+            System.out.println("salah");
+            System.out.println("jawaban yang benar adalah " + angkaRandom);
+            System.out.println("kamu kalah dalam permaian");
+        }
+    }
+}
